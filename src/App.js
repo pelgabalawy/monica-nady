fc7a1cbd9f5a65607222d3a24420d7564ceab2e1
Peter ELgabalawy <pelgabalawy@gmail.com>
2020-03-15 22:08:47 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h3>Monica Nady</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
