05ec4f1d25e34bb40e7653d087c6b394d1053193
peter <pelgabalawy@gmail.com>
2020-04-26 23:45:25 -0400

    finally hooked my code into the firebase database and the firebase storage to pull the images and the data live to the web application

--- src/Section/Section.js
+++ src/Section/Section.js
@@ -1,8 +1,39 @@
 import './Section.css';
 import React from 'react';
 import Preview from '../Preview/Preview.js'
+import firebase from "firebase/app"
+import "firebase/database"
+import "firebase/storage"
 
 class Section extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data:""
+    }
+    this.getData(this.props.name.toLowerCase())
+  }
+
+  getData(section) {
+    var storage = firebase.storage().ref().child(section);  
+    var ref = firebase.database().ref('/').child("products/" + section);
+    var data_l = {}
+    ref.once('value').then(snapshot => {
+      data_l = snapshot.val();
+      if (data_l){
+        this.setState({data: data_l})
+        for (let index=1; index<data_l.length; index++){
+          storage.child(data_l[index].name).getDownloadURL().then(function(url){
+              document.getElementById(section+index.toString()).src=url;
+          }).catch(function(error) {
+            console.log(error)
+          });
+        }
+      }
+      // console.log(this.state)
+    });
+  }
+
   render() {
     let nameAtt = this.props.name;
     return (
@@ -11,7 +42,7 @@ class Section extends React.Component {
       <div id="section" className="card">
         <div className="card-header" id={nameAtt}><h3>{nameAtt}</h3></div>
           <div className="card-body">
-            <Preview name={nameAtt}/>
+            <Preview data={this.state.data} name={nameAtt.toLowerCase()}/>
           </div>
       </div>
       <br/>
